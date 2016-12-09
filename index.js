9bed062fdd385fb0a47cb0c87e28ab807b32d7b7
Roger Yuan <roger.yuan@dhigroupinc.com>
2016-12-09 12:34:27 -0600

    v5

--- index.js
+++ index.js
@@ -2,23 +2,29 @@ var express = require('express');
 
 var app = express();
 
+var UserDb = require("./users.json");
+
 app.get('/', function(req, res){
-	//res.send('hello world');
 	rest.status(200).send('hello world');
 });
 
 app.get('/users', function(req, res){
-	res.send("Hello users");
+	res.type('json').status(200).send(UserDb);
 });	
 
 app.get('/users/:id', function(req, res){
-	res.send("Hello users id:" + req.params.id);
+	var user = UserDb.filter((item)=>{return item.id==req.params.id})[0];
+	if(user != null){
+		res.type('json').status(200).send(user);
+	}
+	else{
+		res.status(404).send("404: Not Found");
+	}
 });	
 
 app.use('*', (req, res)=>{
 	res.status(404).send("404: Response not found");
 });
-
 
 app.listen(8081, function(){
 	console.log('live on 8081');
