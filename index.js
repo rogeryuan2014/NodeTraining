30de2bcefc2e465f8e653b3fe19818e68ff07235
Roger Yuan <roger.yuan@dhigroupinc.com>
2016-12-09 12:20:41 -0600

    v4

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	res.send('hello world');
+	//res.send('hello world');
+	rest.status(200).send('hello world');
 });
 
 app.get('/users', function(req, res){
@@ -14,6 +15,13 @@ app.get('/users/:id', function(req, res){
 	res.send("Hello users id:" + req.params.id);
 });	
 
+app.use('*', (req, res)=>{
+	res.status(404).send("404: Response not found");
+});
+
+
 app.listen(8081, function(){
 	console.log('live on 8081');
 });
+
+
